f5ae076ac5b56205a69b49cf4d897cec8ac84dac
lesching2014 <test@test.com>
2021-07-16 21:28:11 +0800

    touchscreen

--- drivers/input/touchscreen/focaltech_touch/focaltech_ex_mode.c
+++ drivers/input/touchscreen/focaltech_touch/focaltech_ex_mode.c
@@ -42,12 +42,12 @@
 * 3.Private enumerations, structures and unions using typedef
 *****************************************************************************/
 struct fts_mode_flag {
-	int  fts_glove_mode_flag;
-	int  fts_cover_mode_flag;
-	int  fts_charger_mode_flag;
+    int  fts_glove_mode_flag;
+    int  fts_cover_mode_flag;
+    int  fts_charger_mode_flag;
 };
 
-static struct fts_mode_flag g_fts_mode_flag;
+struct fts_mode_flag g_fts_mode_flag;
 
 /*****************************************************************************
 * 4.Static variables
@@ -56,43 +56,58 @@ static struct fts_mode_flag g_fts_mode_flag;
 /*****************************************************************************
 * 5.Global variable or extern global variabls/functions
 *****************************************************************************/
+int fts_enter_glove_mode(struct i2c_client *client, int mode );
+int fts_enter_cover_mode(struct i2c_client *client, int mode );
+int fts_enter_charger_mode(struct i2c_client *client, int mode );
 
 /*****************************************************************************
 * 6.Static function prototypes
 *******************************************************************************/
 
 #if FTS_GLOVE_EN
-static ssize_t fts_touch_glove_show(struct device *dev,
-		struct device_attribute *attr, char *buf)
+static ssize_t fts_touch_glove_show(struct device *dev, struct device_attribute *attr, char *buf)
 {
-	return snprintf(buf, PAGE_SIZE, "Glove: %s\n",
-			g_fts_mode_flag.fts_glove_mode_flag ? "On" : "Off");
+    int count;
+    u8 val;
+    struct input_dev *input_dev = fts_data->input_dev;
+    struct i2c_client *client = container_of(dev, struct i2c_client, dev);
+
+    mutex_lock(&input_dev->mutex);
+    fts_i2c_read_reg(client, FTS_REG_GLOVE_MODE_EN, &val);
+    count = snprintf(buf, PAGE_SIZE, "Glove Mode: %s\n", g_fts_mode_flag.fts_glove_mode_flag ? "On" : "Off");
+    count += snprintf(buf + count, PAGE_SIZE, "Glove Reg(0xC0) = %d\n", val);
+    mutex_unlock(&input_dev->mutex);
+
+    return count;
 }
 
-static ssize_t fts_touch_glove_store(struct device *dev,
-		struct device_attribute *attr, const char *buf, size_t count)
+static ssize_t fts_touch_glove_store(struct device *dev, struct device_attribute *attr, const char *buf, size_t count)
 {
-	int ret;
+    int ret;
+    struct fts_ts_data *ts_data = fts_data;
+    struct i2c_client *client;
 
-	if (FTS_SYSFS_ECHO_ON(buf)) {
-		if (!g_fts_mode_flag.fts_glove_mode_flag) {
-			FTS_INFO("[Mode]enter glove mode");
-			ret = fts_enter_glove_mode(fts_i2c_client, true);
-			if (ret >= 0)
-				g_fts_mode_flag.fts_glove_mode_flag = true;
-		}
-	} else if (FTS_SYSFS_ECHO_OFF(buf)) {
-		if (g_fts_mode_flag.fts_glove_mode_flag) {
-			FTS_INFO("[Mode]exit glove mode");
-			ret = fts_enter_glove_mode(fts_i2c_client, false);
-			if (ret >= 0)
-				g_fts_mode_flag.fts_glove_mode_flag = false;
-		}
-	}
 
-	FTS_INFO("[Mode]glove mode status:  %d",
-			g_fts_mode_flag.fts_glove_mode_flag);
-	return count;
+    client = ts_data->client;
+    if (FTS_SYSFS_ECHO_ON(buf)) {
+        if (!g_fts_mode_flag.fts_glove_mode_flag) {
+            FTS_INFO("[Mode]enter glove mode");
+            ret = fts_enter_glove_mode(client, true);
+            if (ret >= 0) {
+                g_fts_mode_flag.fts_glove_mode_flag = true;
+            }
+        }
+    } else if (FTS_SYSFS_ECHO_OFF(buf)) {
+        if (g_fts_mode_flag.fts_glove_mode_flag) {
+            FTS_INFO("[Mode]exit glove mode");
+            ret = fts_enter_glove_mode(client, false);
+            if (ret >= 0) {
+                g_fts_mode_flag.fts_glove_mode_flag = false;
+            }
+        }
+    }
+    FTS_INFO("[Mode]glove mode status:  %d", g_fts_mode_flag.fts_glove_mode_flag);
+    return count;
 }
 
 /************************************************************************
@@ -102,24 +117,24 @@ static ssize_t fts_touch_glove_store(struct device *dev,
 * Output: no
 * Return: success >=0, otherwise failed
 ***********************************************************************/
-int fts_enter_glove_mode(struct i2c_client *client, int mode)
+int fts_enter_glove_mode( struct i2c_client *client, int mode)
 {
-	int ret = 0;
-	static u8 buf_addr[2] = { 0 };
-	static u8 buf_value[2] = { 0 };
+    int ret = 0;
+    static u8 buf_addr[2] = { 0 };
+    static u8 buf_value[2] = { 0 };
+    buf_addr[0] = FTS_REG_GLOVE_MODE_EN; /* glove control */
 
-	buf_addr[0] = FTS_REG_GLOVE_MODE_EN; /* glove control */
+    if (mode)
+        buf_value[0] = 0x01;
+    else
+        buf_value[0] = 0x00;
 
-	if (mode)
-		buf_value[0] = 0x01;
-	else
-		buf_value[0] = 0x00;
+    ret = fts_i2c_write_reg( client, buf_addr[0], buf_value[0]);
+    if (ret < 0) {
+        FTS_ERROR("[Mode]fts_enter_glove_mode write value fail");
+    }
 
-	ret = fts_i2c_write_reg(client, buf_addr[0], buf_value[0]);
-	if (ret < 0)
-		FTS_ERROR("[Mode]fts_enter_glove_mode write value fail");
-
-	return ret;
+    return ret ;
 
 }
 
@@ -128,43 +143,53 @@ int fts_enter_glove_mode(struct i2c_client *client, int mode)
 *   write example:echo 01 > fts_touch_glove_mode ---write glove mode to 01
 *
 */
-static DEVICE_ATTR(fts_glove_mode, S_IRUGO|S_IWUSR,
-		fts_touch_glove_show, fts_touch_glove_store);
+static DEVICE_ATTR (fts_glove_mode,  S_IRUGO | S_IWUSR, fts_touch_glove_show, fts_touch_glove_store);
 
 #endif
 
 #if FTS_COVER_EN
-static ssize_t fts_touch_cover_show(struct device *dev,
-		struct device_attribute *attr, char *buf)
+static ssize_t fts_touch_cover_show(struct device *dev, struct device_attribute *attr, char *buf)
 {
-	return snprintf(buf, PAGE_SIZE, "Cover: %s\n",
-			g_fts_mode_flag.fts_cover_mode_flag ? "On" : "Off");
+    int count;
+    u8 val;
+    struct input_dev *input_dev = fts_data->input_dev;
+    struct i2c_client *client = container_of(dev, struct i2c_client, dev);
+
+    mutex_lock(&input_dev->mutex);
+    fts_i2c_read_reg(client, FTS_REG_COVER_MODE_EN, &val);
+    count = snprintf(buf, PAGE_SIZE, "Cover Mode: %s\n", g_fts_mode_flag.fts_cover_mode_flag ? "On" : "Off");
+    count += snprintf(buf + count, PAGE_SIZE, "Cover Reg(0xC1) = %d\n", val);
+    mutex_unlock(&input_dev->mutex);
+
+    return count;
 }
 
-static ssize_t fts_touch_cover_store(struct device *dev,
-		struct device_attribute *attr, const char *buf, size_t count)
+static ssize_t fts_touch_cover_store(struct device *dev, struct device_attribute *attr, const char *buf, size_t count)
 {
-	int ret;
+    int ret;
+    struct fts_ts_data *ts_data = fts_data;
+    struct i2c_client *client;
 
-	if (FTS_SYSFS_ECHO_ON(buf)) {
-		if (!g_fts_mode_flag.fts_cover_mode_flag) {
-			FTS_INFO("[Mode]enter cover mode");
-			ret = fts_enter_cover_mode(fts_i2c_client, true);
-			if (ret >= 0)
-				g_fts_mode_flag.fts_cover_mode_flag = true;
-		}
-	} else if (FTS_SYSFS_ECHO_OFF(buf)) {
-		if (g_fts_mode_flag.fts_cover_mode_flag) {
-			FTS_INFO("[Mode]exit cover mode");
-			ret = fts_enter_cover_mode(fts_i2c_client, false);
-			if (ret >= 0)
-				g_fts_mode_flag.fts_cover_mode_flag = false;
-		}
-	}
-
-	FTS_INFO("[Mode]cover mode status:  %d",
-			g_fts_mode_flag.fts_cover_mode_flag);
-	return count;
+    client = ts_data->client;
+    if (FTS_SYSFS_ECHO_ON(buf)) {
+        if (!g_fts_mode_flag.fts_cover_mode_flag) {
+            FTS_INFO("[Mode]enter cover mode");
+            ret = fts_enter_cover_mode(client, true);
+            if (ret >= 0) {
+                g_fts_mode_flag.fts_cover_mode_flag = true;
+            }
+        }
+    } else if (FTS_SYSFS_ECHO_OFF(buf)) {
+        if (g_fts_mode_flag.fts_cover_mode_flag) {
+            FTS_INFO("[Mode]exit cover mode");
+            ret = fts_enter_cover_mode(client, false);
+            if (ret >= 0) {
+                g_fts_mode_flag.fts_cover_mode_flag = false;
+            }
+        }
+    }
+    FTS_INFO("[Mode]cover mode status:  %d", g_fts_mode_flag.fts_cover_mode_flag);
+    return count;
 }
 
 /************************************************************************
@@ -174,24 +199,24 @@ static ssize_t fts_touch_cover_store(struct device *dev,
 * Output: no
 * Return: success >=0, otherwise failed
 ***********************************************************************/
-int  fts_enter_cover_mode(struct i2c_client *client, int mode)
+int  fts_enter_cover_mode( struct i2c_client *client, int mode)
 {
-	int ret = 0;
-	static u8 buf_addr[2] = { 0 };
-	static u8 buf_value[2] = { 0 };
+    int ret = 0;
+    static u8 buf_addr[2] = { 0 };
+    static u8 buf_value[2] = { 0 };
+    buf_addr[0] = FTS_REG_COVER_MODE_EN; /* cover control */
 
-	buf_addr[0] = FTS_REG_COVER_MODE_EN; /* cover control */
+    if (mode)
+        buf_value[0] = 0x01;
+    else
+        buf_value[0] = 0x00;
 
-	if (mode)
-		buf_value[0] = 0x01;
-	else
-		buf_value[0] = 0x00;
+    ret = fts_i2c_write_reg( client, buf_addr[0], buf_value[0]);
+    if (ret < 0) {
+        FTS_ERROR("[Mode] fts_enter_cover_mode write value fail \n");
+    }
 
-	ret = fts_i2c_write_reg(client, buf_addr[0], buf_value[0]);
-	if (ret < 0)
-		FTS_ERROR("[Mode] fts_enter_cover_mode write value fail\n");
-
-	return ret;
+    return ret ;
 
 }
 
@@ -200,43 +225,54 @@ int  fts_enter_cover_mode(struct i2c_client *client, int mode)
 *   write example:echo 01 > fts_touch_cover_mode ---write cover mode to 01
 *
 */
-static DEVICE_ATTR(fts_cover_mode, S_IRUGO|S_IWUSR,
-		fts_touch_cover_show, fts_touch_cover_store);
+static DEVICE_ATTR (fts_cover_mode,  S_IRUGO | S_IWUSR, fts_touch_cover_show, fts_touch_cover_store);
 
 #endif
 
 #if FTS_CHARGER_EN
-static ssize_t fts_touch_charger_show(struct device *dev,
-		struct device_attribute *attr, char *buf)
+static ssize_t fts_touch_charger_show(struct device *dev, struct device_attribute *attr, char *buf)
 {
-	return snprintf(buf, PAGE_SIZE, "Charger: %s\n",
-			g_fts_mode_flag.fts_charger_mode_flag ? "On" : "Off");
+    int count;
+    u8 val;
+    struct input_dev *input_dev = fts_data->input_dev;
+    struct i2c_client *client = container_of(dev, struct i2c_client, dev);
+
+    mutex_lock(&input_dev->mutex);
+    fts_i2c_read_reg(client, FTS_REG_CHARGER_MODE_EN, &val);
+    count = snprintf(buf, PAGE_SIZE, "Charge Mode: %s\n", g_fts_mode_flag.fts_charger_mode_flag ? "On" : "Off");
+    count += snprintf(buf + count, PAGE_SIZE, "Charge Reg(0x8B) = %d\n", val);
+    mutex_unlock(&input_dev->mutex);
+
+    return count;
 }
 
-static ssize_t fts_touch_charger_store(struct device *dev,
-		struct device_attribute *attr, const char *buf, size_t count)
+static ssize_t fts_touch_charger_store(struct device *dev, struct device_attribute *attr, const char *buf, size_t count)
 {
-	int ret;
+    int ret;
+    struct fts_ts_data *ts_data = fts_data;
+    struct i2c_client *client;
 
-	if (FTS_SYSFS_ECHO_ON(buf)) {
-		if (!g_fts_mode_flag.fts_charger_mode_flag) {
-			FTS_INFO("[Mode]enter charger mode");
-			ret = fts_enter_charger_mode(fts_i2c_client, true);
-			if (ret >= 0)
-				g_fts_mode_flag.fts_charger_mode_flag = true;
-		}
-	} else if (FTS_SYSFS_ECHO_OFF(buf)) {
-		if (g_fts_mode_flag.fts_charger_mode_flag) {
-			FTS_INFO("[Mode]exit charger mode");
-			ret = fts_enter_charger_mode(fts_i2c_client, false);
-			if (ret >= 0)
-				g_fts_mode_flag.fts_charger_mode_flag = false;
-		}
-	}
+    client = ts_data->client;
 
-	FTS_INFO("[Mode]charger mode status: %d",
-			g_fts_mode_flag.fts_charger_mode_flag);
-	return count;
+    if (FTS_SYSFS_ECHO_ON(buf)) {
+        if (!g_fts_mode_flag.fts_charger_mode_flag) {
+            FTS_INFO("[Mode]enter charger mode");
+            ret = fts_enter_charger_mode(client, true);
+            if (ret >= 0) {
+                g_fts_mode_flag.fts_charger_mode_flag = true;
+            }
+        }
+    } else if (FTS_SYSFS_ECHO_OFF(buf)) {
+        if (g_fts_mode_flag.fts_charger_mode_flag) {
+            FTS_INFO("[Mode]exit charger mode");
+            ret = fts_enter_charger_mode(client, false);
+            if (ret >= 0) {
+                g_fts_mode_flag.fts_charger_mode_flag = false;
+            }
+        }
+    }
+    FTS_INFO("[Mode]charger mode status: %d", g_fts_mode_flag.fts_charger_mode_flag);
+    return count;
 }
 
 /************************************************************************
@@ -248,22 +284,22 @@ static ssize_t fts_touch_charger_store(struct device *dev,
 ***********************************************************************/
 int  fts_enter_charger_mode(struct i2c_client *client, int mode)
 {
-	int ret = 0;
-	static u8 buf_addr[2] = { 0 };
-	static u8 buf_value[2] = { 0 };
+    int ret = 0;
+    static u8 buf_addr[2] = { 0 };
+    static u8 buf_value[2] = { 0 };
+    buf_addr[0] = FTS_REG_CHARGER_MODE_EN; /* charger control */
 
-	buf_addr[0] = FTS_REG_CHARGER_MODE_EN; /* charger control */
+    if (mode)
+        buf_value[0] = 0x01;
+    else
+        buf_value[0] = 0x00;
 
-	if (mode)
-		buf_value[0] = 0x01;
-	else
-		buf_value[0] = 0x00;
+    ret = fts_i2c_write_reg( client, buf_addr[0], buf_value[0]);
+    if (ret < 0) {
+        FTS_DEBUG("[Mode]fts_enter_charger_mode write value fail");
+    }
 
-	ret = fts_i2c_write_reg(client, buf_addr[0], buf_value[0]);
-	if (ret < 0)
-		FTS_DEBUG("[Mode]fts_enter_charger_mode write value fail");
-
-	return ret;
+    return ret ;
 
 }
 
@@ -272,76 +308,75 @@ int  fts_enter_charger_mode(struct i2c_client *client, int mode)
 *   write example:echo 01 > fts_touch_charger_mode ---write charger mode to 01
 *
 */
-static DEVICE_ATTR(fts_charger_mode, S_IRUGO|S_IWUSR,
-		fts_touch_charger_show, fts_touch_charger_store);
+static DEVICE_ATTR (fts_charger_mode,  S_IRUGO | S_IWUSR, fts_touch_charger_show, fts_touch_charger_store);
 
 #endif
 
 static struct attribute *fts_touch_mode_attrs[] = {
 #if FTS_GLOVE_EN
-	&dev_attr_fts_glove_mode.attr,
+    &dev_attr_fts_glove_mode.attr,
 #endif
 
 #if FTS_COVER_EN
-	&dev_attr_fts_cover_mode.attr,
+    &dev_attr_fts_cover_mode.attr,
 #endif
 
 #if FTS_CHARGER_EN
-	&dev_attr_fts_charger_mode.attr,
+    &dev_attr_fts_charger_mode.attr,
 #endif
 
-	NULL,
+    NULL,
 };
 
 static struct attribute_group fts_touch_mode_group = {
-	.attrs = fts_touch_mode_attrs,
+    .attrs = fts_touch_mode_attrs,
 };
 
 int fts_ex_mode_init(struct i2c_client *client)
 {
-	int err = 0;
+    int err = 0;
 
-	g_fts_mode_flag.fts_glove_mode_flag = false;
-	g_fts_mode_flag.fts_cover_mode_flag = false;
-	g_fts_mode_flag.fts_charger_mode_flag = false;
+    g_fts_mode_flag.fts_glove_mode_flag = false;
+    g_fts_mode_flag.fts_cover_mode_flag = false;
+    g_fts_mode_flag.fts_charger_mode_flag = false;
 
-	err = sysfs_create_group(&client->dev.kobj, &fts_touch_mode_group);
-	if (0 != err) {
-		FTS_ERROR("[Mode]create sysfs failed.");
-		sysfs_remove_group(&client->dev.kobj, &fts_touch_mode_group);
-		return -EIO;
-	}
+    err = sysfs_create_group(&client->dev.kobj, &fts_touch_mode_group);
+    if (0 != err) {
+        FTS_ERROR("[Mode]create sysfs failed.");
+        sysfs_remove_group(&client->dev.kobj, &fts_touch_mode_group);
+        return -EIO;
+    } else {
+        FTS_DEBUG("[Mode]create sysfs succeeded");
+    }
 
-	FTS_DEBUG("[Mode]create sysfs succeeded");
-
-	return err;
+    return err;
 
 }
 
 int fts_ex_mode_exit(struct i2c_client *client)
 {
-	sysfs_remove_group(&client->dev.kobj, &fts_touch_mode_group);
-	return 0;
+    sysfs_remove_group(&client->dev.kobj, &fts_touch_mode_group);
+    return 0;
 }
 
 int fts_ex_mode_recovery(struct i2c_client *client)
 {
-	int ret = 0;
+    int ret = 0;
 #if FTS_GLOVE_EN
-	if (g_fts_mode_flag.fts_glove_mode_flag)
-		ret = fts_enter_glove_mode(client, true);
+    if (g_fts_mode_flag.fts_glove_mode_flag)
+        ret = fts_enter_glove_mode(client, true);
 #endif
 
 #if FTS_COVER_EN
-	if (g_fts_mode_flag.fts_cover_mode_flag)
-		ret = fts_enter_cover_mode(client, true);
+    if (g_fts_mode_flag.fts_cover_mode_flag)
+        ret = fts_enter_cover_mode(client, true);
 #endif
 
 #if FTS_CHARGER_EN
-	if (g_fts_mode_flag.fts_charger_mode_flag)
-		ret = fts_enter_charger_mode(client, true);
+    if (g_fts_mode_flag.fts_charger_mode_flag)
+        ret = fts_enter_charger_mode(client, true);
 #endif
 
-	return ret;
+    return ret;
 }
 
--- drivers/input/touchscreen/focaltech_touch/focaltech_i2c.c
+++ drivers/input/touchscreen/focaltech_touch/focaltech_i2c.c
@@ -40,7 +40,7 @@
 /*****************************************************************************
 * Private constant and macro definitions using #define
 *****************************************************************************/
-
+#define I2C_RETRY_NUMBER        3
 /*****************************************************************************
 * Private enumerations, structures and unions using typedef
 *****************************************************************************/
@@ -69,50 +69,57 @@ static DEFINE_MUTEX(i2c_rw_access);
 * Output: get data in the 3rd buf
 * Return: fail <0
 ***********************************************************************/
-int fts_i2c_read(struct i2c_client *client, char *writebuf,
-		int writelen, char *readbuf, int readlen)
+int fts_i2c_read(struct i2c_client *client, char *writebuf, int writelen, char *readbuf, int readlen)
 {
-	int ret = -EIO;
+    int ret = 0;
+    int i = 0;
 
-	mutex_lock(&i2c_rw_access);
+    mutex_lock(&i2c_rw_access);
 
-	if (readlen > 0) {
-		if (writelen > 0) {
-			struct i2c_msg msgs[] = {
-				{
-					.addr = client->addr,
-					.flags = 0,
-					.len = writelen,
-					.buf = writebuf,
-				},
-				{
-					.addr = client->addr,
-					.flags = I2C_M_RD,
-					.len = readlen,
-					.buf = readbuf,
-				},
-			};
-			ret = i2c_transfer(client->adapter, msgs, 2);
-			if (ret < 0)
-				FTS_ERROR("[IIC]: i2c_write error %d!!", ret);
-		} else {
-			struct i2c_msg msgs[] = {
-				{
-					.addr = client->addr,
-					.flags = I2C_M_RD,
-					.len = readlen,
-					.buf = readbuf,
-				},
-			};
-			ret = i2c_transfer(client->adapter, msgs, 1);
-			if (ret < 0)
-				FTS_ERROR("[IIC]: i2c_read error %d!!", ret);
-		}
-	}
+    if (readlen > 0) {
+        if (writelen > 0) {
+            struct i2c_msg msgs[] = {
+                {
+                    .addr = client->addr,
+                    .flags = 0,
+                    .len = writelen,
+                    .buf = writebuf,
+                },
+                {
+                    .addr = client->addr,
+                    .flags = I2C_M_RD,
+                    .len = readlen,
+                    .buf = readbuf,
+                },
+            };
+            for (i = 0; i < I2C_RETRY_NUMBER; i++) {
+                ret = i2c_transfer(client->adapter, msgs, 2);
+                if (ret < 0) {
+                    FTS_ERROR("[IIC]: i2c_transfer(write) error, ret=%d!!", ret);
+                } else
+                    break;
+            }
+        } else {
+            struct i2c_msg msgs[] = {
+                {
+                    .addr = client->addr,
+                    .flags = I2C_M_RD,
+                    .len = readlen,
+                    .buf = readbuf,
+                },
+            };
+            for (i = 0; i < I2C_RETRY_NUMBER; i++) {
+                ret = i2c_transfer(client->adapter, msgs, 1);
+                if (ret < 0) {
+                    FTS_ERROR("[IIC]: i2c_transfer(read) error, ret=%d!!", ret);
+                } else
+                    break;
+            }
+        }
+    }
 
-	mutex_unlock(&i2c_rw_access);
-
-	return ret;
+    mutex_unlock(&i2c_rw_access);
+    return ret;
 }
 
 /************************************************************************
@@ -124,27 +131,30 @@ int fts_i2c_read(struct i2c_client *client, char *writebuf,
 ***********************************************************************/
 int fts_i2c_write(struct i2c_client *client, char *writebuf, int writelen)
 {
-	int ret = 0;
+    int ret = 0;
+    int i = 0;
 
-	mutex_lock(&i2c_rw_access);
+    mutex_lock(&i2c_rw_access);
+    if (writelen > 0) {
+        struct i2c_msg msgs[] = {
+            {
+                .addr = client->addr,
+                .flags = 0,
+                .len = writelen,
+                .buf = writebuf,
+            },
+        };
+        for (i = 0; i < I2C_RETRY_NUMBER; i++) {
+            ret = i2c_transfer(client->adapter, msgs, 1);
+            if (ret < 0) {
+                FTS_ERROR("%s: i2c_transfer(write) error, ret=%d", __func__, ret);
+            } else
+                break;
+        }
+    }
+    mutex_unlock(&i2c_rw_access);
 
-	if (writelen > 0) {
-		struct i2c_msg msgs[] = {
-			{
-				.addr = client->addr,
-				.flags = 0,
-				.len = writelen,
-				.buf = writebuf,
-			},
-		};
-		ret = i2c_transfer(client->adapter, msgs, 1);
-		if (ret < 0)
-			FTS_ERROR("[IIC]: i2c_write error, ret=%d", ret);
-	}
-
-	mutex_unlock(&i2c_rw_access);
-
-	return ret;
+    return ret;
 }
 
 /************************************************************************
@@ -156,11 +166,11 @@ int fts_i2c_write(struct i2c_client *client, char *writebuf, int writelen)
 ***********************************************************************/
 int fts_i2c_write_reg(struct i2c_client *client, u8 regaddr, u8 regvalue)
 {
-	u8 buf[2] = {0};
+    u8 buf[2] = {0};
 
-	buf[0] = regaddr;
-	buf[1] = regvalue;
-	return fts_i2c_write(client, buf, sizeof(buf));
+    buf[0] = regaddr;
+    buf[1] = regvalue;
+    return fts_i2c_write(client, buf, sizeof(buf));
 }
 
 /************************************************************************
@@ -172,7 +182,32 @@ int fts_i2c_write_reg(struct i2c_client *client, u8 regaddr, u8 regvalue)
 ***********************************************************************/
 int fts_i2c_read_reg(struct i2c_client *client, u8 regaddr, u8 *regvalue)
 {
-	return fts_i2c_read(client, &regaddr, 1, regvalue, 1);
+    return fts_i2c_read(client, &regaddr, 1, regvalue, 1);
+}
+
+/************************************************************************
+* HID to standard I2C
+***********************************************************************/
+void fts_i2c_hid2std(struct i2c_client *client)
+{
+    int ret = 0;
+    u8 buf[3] = {0xeb, 0xaa, 0x09};
+
+    ret = fts_i2c_write(client, buf, 3);
+    if (ret < 0)
+        FTS_ERROR("hid2std cmd write fail");
+    else {
+        msleep(10);
+        buf[0] = buf[1] = buf[2] = 0;
+        ret = fts_i2c_read(client, NULL, 0, buf, 3);
+        if (ret < 0)
+            FTS_ERROR("hid2std cmd read fail");
+        else if ((0xeb == buf[0]) && (0xaa == buf[1]) && (0x08 == buf[2])) {
+            FTS_DEBUG("hidi2c change to stdi2c successful");
+        } else {
+            FTS_ERROR("hidi2c change to stdi2c fail");
+        }
+    }
 }
 
 /************************************************************************
@@ -184,10 +219,10 @@ int fts_i2c_read_reg(struct i2c_client *client, u8 regaddr, u8 *regvalue)
 ***********************************************************************/
 int fts_i2c_init(void)
 {
-	FTS_FUNC_ENTER();
+    FTS_FUNC_ENTER();
 
-	FTS_FUNC_EXIT();
-	return 0;
+    FTS_FUNC_EXIT();
+    return 0;
 }
 /************************************************************************
 * Name: fts_i2c_exit
@@ -198,9 +233,9 @@ int fts_i2c_init(void)
 ***********************************************************************/
 int fts_i2c_exit(void)
 {
-	FTS_FUNC_ENTER();
+    FTS_FUNC_ENTER();
 
-	FTS_FUNC_EXIT();
-	return 0;
+    FTS_FUNC_EXIT();
+    return 0;
 }
 
